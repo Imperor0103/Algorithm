0ca3f0b0cd22e1a083a5230867fb1bf9e3522180
Imperor <119866946+Imperor0103@users.noreply.github.com>
2025-10-30 05:40:02 +0900

    [Bronze IV] Title: 다면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int T;
+	cin >> T;
+
+	while (T--) 
+	{
+		int V, E;
+		cin >> V >> E;
+		cout << 2 - V + E << '\n';
+	}
+	return 0;
+}
